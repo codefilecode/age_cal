446d48a2369a586065cd8d718bf132fff2bf8848
cpu04 <zf0908@naver.com>
2022-07-15 16:35:46 +0900

    upload

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ def knowage():
     print("기준 년도를 적어주세요")
     year = int(input())
 
-    yearA = year - age # 생일전
-    yearB = year - age - 1 # 생일후
+    yearA = year - age # 생일전년도
+    yearB = year - age - 1 # 생일후년도
     ageA = year - yearA # 생일전
     ageB = year - yearB # 생일후
-    iA = ageA + 1
-    iB = ageB + 1
-    iyA = yearA + 1
-    iyB = yearA + 1
+
 
     print(year,"년을 기준으로 나이는 다음과 같습니다.")
     print("생일전 또는 생일년도 나이면",yearA,"년도, 생일후 나이면",yearB,"년도 입니다.")
     print("생일전의 나이면",ageA,"세, 생일후의 나이면",ageB,"세 입니다.")
     print("세는 나이는",year,"년을 기준으로 다음과 같습니다.")
-    print("세는 나이는",year+1-age,"세 입니다.")
+    print("세는 나이는",year-n+1,"세 입니다.")
 
 def knowageyear():
     print("만나이는 %d세 입니다.")
