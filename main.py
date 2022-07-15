b7798b05e6474120c04bab35882e35284ba7e090
cpu04 <zf0908@naver.com>
2022-07-15 18:07:00 +0900

    upload

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 def knowage(i):
+    print("기준 년도를 적어주세요")
+    year = int(input())
+
     yearA = year - i # 생일전년도
     yearB = year - i - 1 # 생일후년도
     ageA = year - yearA # 생일전
     ageB = year - yearB # 생일후
     iyearA = year - i + 1 # 세는나이 연도 # 1번 라인
 
-    print("기준 년도를 적어주세요")
-    year = int(input())
-
-    print(year,"년을 기준으로 만 나이는 다음과 같습니다.")
+    print("%d년을 기준으로 만 나이는 다음과 같습니다." % year)
     print("생일전 생일년도: %d년생" % yearA)
     print("생일후 생일년도: %d년생" % yearB)
     print("생일전 나이: %d세" % ageA)
     print("생일후 나이: %d세" % ageB)
-    print(year,"년을 기준으로 세는 나이는 다음과 같습니다.")
+    print("%d년을 기준으로 세는 나이는 다음과 같습니다." % year)
     print("생년년도: %d년생" % iyearA)
     print("나이: %d세" % i)
 
 def knowageyear(i): # 2번 라인
-    print("기준 년도를 적어주세요")
-    year = int(input())
     print("태어난 년도를 적어주세요")
-    yearb = int(input())
-    ageA = year - yearb
-    ageB = year - yearb - 1
-    print(year,"년을 기준으로 만 나이는 다음과 같습니다.")
+    year = int(input())
+    ageA = year - i - 1
+    ageB = year - i
+    print("%d년을 기준으로 만 나이는 다음과 같습니다." % year)
     print("생일전 생년월일: %d세" % ageA)
     print("생일후 생년월일: %d세" % ageB)
+
 
 def knowagemouth(i):
     print("hello")
