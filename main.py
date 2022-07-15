cc822906297d587e442e4d910034ab977e898500
cpu04 <zf0908@naver.com>
2022-07-15 14:05:27 +0900

    upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-def knowage(i):
-    print("년도를 적어주세요")
+def knowage():
+    age = int(input("나이를 입력해주세요 : "))
+    print("기준 년도를 적어주세요")
     year = int(input())
 
-    print("태어난 년도는 %d년 ~ %d년입니다.")
-    print("만 나이는 %d세 ~ %d세 입니다.")
-    print("한국식 세는 나이는 %d세 입니다.")
+    yeara = year - age
+    yearb = year - age + 1
+
+    print(year+"년을 기준으로 나이는 다음과 같습니다.")
+    print("생일전 기준 나이는 "+yeara+"세 입니다.")
+    print("생일후 기준 나이는 "+yeara+"세 입니다.")
+
 def knowageyear():
 
 def knowagemouth():
@@ -15,9 +20,6 @@ def knowageday():
 
 
 print(" === 만나이 계산기 === ")
-
-age = int(input("나이를 입력해주세요 : "))
-
 print("다음중 어느것에 해당하십니까?")
 print("1 : 나이만 알고 있음")
 print("2 : 년도만 알고 있음")
@@ -26,7 +28,7 @@ print("4 : 년월일을 알고 있음")
 j = int(input())
 
 if(j == 1):
-    knowage(age)
+    knowage()
 elif(j == 2):
     knowageyear()
 elif(j == 3):
