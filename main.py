7bc2913f781e9c03e4cdb7565688c6ad8aeae5c1
cpu04 <zf0908@naver.com>
2022-07-15 13:06:27 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# def knowage():
+
+# def knowageyear():
+
+# def knowagemouth():
+
+# def knowageday():
+
+print(" === 만나이 계산기 === ")
+
+i = int(input("나이를 입력해주세요. : "))
+
+print("다음중 어느것에 해당하십니까?")
+print("1. 나이만 알고 있음")
+print("2. 년도만 알고 있음")
+print("3. 년도 월만 알고 있음")
+print("4. 년,월,일을 알고 있음")
