4b72ed1d339b200d34ea049addcd9564b2c5beb9
cpu04 <zf0908@naver.com>
2022-07-15 17:45:38 +0900

    upload

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ def knowage(i):
     print("나이: %d세" % i)
 
 def knowageyear(i): # 2번 라인
+    ageA = year - yearA # 생일전
+    ageB = year - yearB # 생일후
+
     print("기준 년도를 적어주세요")
     year = int(input())
     print(year,"년을 기준으로 만 나이는 다음과 같습니다.")
-    print()
+    print("생일전 생년월일: %d세" % ageA)
+    print("생일후 생년월일: %d세" % ageB)
 def knowagemouth(i):
 
 def knowageday(i):
@@ -34,7 +38,7 @@ age = int(input("나이를 입력해주세요 : "))
 
 print("다음중 어느것에 해당하십니까?")
 print("1 : 나이만 알고 있음")
-print("2 : 년도만 알고 있음")
+print("2 : 나이와, 년도만 알고 있음")
 print("3 : 년도 월만 알고 있음")
 print("4 : 년월일을 알고 있음")
 j = int(input())
