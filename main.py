53d837fedb33e73e7806b1cbb07621d66a1cfa2a
cpu04 <zf0908@naver.com>
2022-07-15 20:18:31 +0900

    upload

--- main.py
+++ main.py
@@ -25,16 +25,15 @@ def knowageyear(i): # 2번 라인
 
     ageA = year - yearb - 1
     ageB = year - yearb
-    iyearA = yearb - i + 1
+    iyearA = yearb + i - 1
 
-    print("%d년을 기준으로 만 나이는 다음과 같습니다." % year)
+    print("%d년생을 기준으로 만 나이는 다음과 같습니다." % year)
     print("생일전 생년월일: %d세" % ageA)
     print("생일후 생년월일: %d세" % ageB)
-    print("%d년을 기준으로 세는 나이는 다음과 같습니다." % year)
-    print("생년년도: %d년생" % iyearA)
+    print("%d년생 기준으로 세는 나이는 다음과 같습니다." % year)
     print("나이: %d세" % i)
 def knowagemouth(i):
-
+    print("hello")
 def knowageday(i):
     print("hello")
 
